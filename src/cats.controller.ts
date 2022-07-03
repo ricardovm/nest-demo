265b4b2104753c55af7574f2359cf7c461048163
Ricardo Mannrich <mannrich@hotmail.com>
2022-07-03 19:09:07 -0300

    style: code re-format

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -1,21 +1,21 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { CreateCatDto } from "./create-cat.dto";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-    @Post()
-    async create(@Body() createCatDto: CreateCatDto) {
-        return `This action adds a new cat: ${JSON.stringify(createCatDto)}`;
-    }
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return `This action adds a new cat: ${JSON.stringify(createCatDto)}`;
+  }
 
-    @Get(':id')
-    findOne(@Param() params): string {
-        console.log(params.id);
-        return `This action returns a #${params.id} cat`;
-    }
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
 
-    @Get()
-    findAll(): string {
-        return 'This action returns all cats';
-    }
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
 }
